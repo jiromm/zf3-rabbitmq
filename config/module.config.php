6947f28a8592493b1388b8fa61027dcd89e60ba7
Pele Odiase <pele.odiase@valtech.co.uk>
2016-12-10 17:18:11 +0000

    Move closure from config to Module.php so that config can be cached

--- config/module.config.php
+++ config/module.config.php
@@ -1,16 +1,3 @@
 <?php
 
-use Interop\Container\ContainerInterface;
-use RabbitMQ\Service\RabbitMQ;
-
-return [
-    'service_manager' => [
-        'factories' => [
-            \RabbitMQ\Service\RabbitMQ::class => function (ContainerInterface $container) {
-                $config = $container->get('config');
-
-                return new RabbitMQ($config['rabbitmq']);
-            },
-        ],
-    ],
-];
+return [];
--- src/Module.php
+++ src/Module.php
@@ -2,10 +2,29 @@
 
 namespace RabbitMQ;
 
+use Interop\Container\ContainerInterface;
+use RabbitMQ\Service\RabbitMQ;
+
 class Module
 {
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
+    }
+
+    /**
+     * @return array
+     */
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                RabbitMQ::class => function (ContainerInterface $container) {
+                    $config = $container->get('config');
+
+                    return new RabbitMQ($config['rabbitmq']);
+                },
+            ],
+        ];
     }
 }
